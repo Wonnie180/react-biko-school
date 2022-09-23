9e35e329c37a51c08e5ed9a33bebc6f0b75c9c34
Luis José Llamas Pérez <luisjllamasperez@gmail.com>
2022-09-23 18:23:58 +0200

    Solución

--- src/ui/Home/Home.js
+++ src/ui/Home/Home.js
@@ -1,33 +1,24 @@
-import React, { useState } from 'react'
-import './Home.css'
-
-const comics = [
-  {
-    id: 45977,
-    title: 'Captain America (2012) #11',
-    characters: ['Captain America']
-  },
-  {
-    id: 43722,
-    title: 'Captain America (2012) #1',
-    characters: ['Captain America']
-  },
-  {
-    id: 40391,
-    title: 'Captain America (2011) #18',
-    characters: ['Captain America']
-  },
-  {
-    id: 43339,
-    title: 'Uncanny Avengers (2012) #1',
-    characters: ['Captain America', 'Havok', 'Rogue', 'Scarlet Witch', 'Thor', 'Wolverine']
-  }
-]
+import React, { useState, useEffect } from "react";
+import "./Home.css";
+import { api } from "../../api";
 
 export const Home = () => {
-  const [filter, setFilter] = useState('')
+  const [comics, setComics] = useState([]);
 
-  const filteredComics = comics.filter(comic => comic.title.toLowerCase().includes(filter.toLowerCase()))
+  const fetchData = async () => {
+    const data = await api.allComics();
+    setComics(data);
+  };
+
+  useEffect(() => {
+    fetchData().catch(console.error);
+  }, []);
+
+  const [filter, setFilter] = useState("");
+
+  const filteredComics = comics.filter((comic) =>
+    comic.title.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
     <main className="container">
@@ -35,54 +26,62 @@ export const Home = () => {
       <ComicList comics={filteredComics} filter={filter} onFilter={setFilter} />
       <Footer itemsCount={filteredComics.length} />
     </main>
-  )
-}
+  );
+};
 
 export const Header = () => {
   return (
     <header>
-      <h1 className="title">
-        Buscador de cómics de Marvel
-      </h1>
+      <h1 className="title">Buscador de cómics de Marvel</h1>
       <h2 className="subtitle">
-        Este buscador encontrará los cómics en los que aparezcan los dos personajes que selecciones en el formulario
+        Este buscador encontrará los cómics en los que aparezcan los dos
+        personajes que selecciones en el formulario
       </h2>
     </header>
-  )
-}
+  );
+};
 
 export const ComicList = ({ filter, onFilter, comics }) => {
+  const colors = ["#92c952", "#771796", "#24f355", "#d32776", "#f66b97"];
+  const [value, setValue] = React.useState(0);
+
+  useEffect(() => {
+    setValue((v) => {
+      return v === 4 ? 0 : v + 1;
+    });
+  }, [filter]);
+
   return (
     <section>
-      <p className="inputLabel">
-        Escribe un personaje en la lista
-      </p>
+      <p className="inputLabel">Escribe un personaje en la lista</p>
       <div className="inputContainer">
-        <input className="filterInput" onInput={e => onFilter(e.target.value)} value={filter} />
-        <button className="clearButton" onClick={() => onFilter('')}>Limpiar búsqueda</button>
+        <input
+          className="filterInput"
+          onInput={(e) => onFilter(e.target.value)}
+          value={filter}
+        />
+        <button className="clearButton" onClick={() => onFilter("")}>
+          Limpiar búsqueda
+        </button>
       </div>
-      {comics.map(comic => (
-        <div key={comic.id} className="comicCard">
-          <p className="comicTitle">
-            {comic.title}
-          </p>
-          <p>{comic.characters.join(', ')}</p>
+      {comics.map((comic) => (
+        <div
+          key={comic.id}
+          className="comicCard"
+          style={{ backgroundColor: colors[value] }}
+        >
+          <p className="comicTitle">{comic.title}</p>
+          <p>{comic.characters.join(", ")}</p>
         </div>
       ))}
     </section>
-  )
-}
+  );
+};
 
 export const Footer = ({ itemsCount }) => {
   return (
     <footer>
       <p>Elementos en la lista: {itemsCount}</p>
     </footer>
-  )
-}
-
-
-
-
-
-
+  );
+};

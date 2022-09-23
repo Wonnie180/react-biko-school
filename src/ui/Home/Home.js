6bddd41e936040204071372a92cb549360983981
Luis José Llamas Pérez <luisjllamasperez@gmail.com>
2022-09-23 17:40:34 +0200

    Step-2 inicial

--- src/ui/Home/Home.js
+++ src/ui/Home/Home.js
@@ -1,81 +1,88 @@
-import React from "react";
-import "./Home.css";
-import { useState } from "react";
-import { SearchBox } from "../../Components/SearchBox/SearchBox";
+import React, { useState } from 'react'
+import './Home.css'
 
 const comics = [
   {
     id: 45977,
-    title: "Captain America (2012) #11",
-    characters: ["Captain America"],
+    title: 'Captain America (2012) #11',
+    characters: ['Captain America']
   },
   {
     id: 43722,
-    title: "Captain America (2012) #1",
-    characters: ["Captain America"],
+    title: 'Captain America (2012) #1',
+    characters: ['Captain America']
   },
   {
     id: 40391,
-    title: "Captain America (2011) #18",
-    characters: ["Captain America"],
+    title: 'Captain America (2011) #18',
+    characters: ['Captain America']
   },
   {
     id: 43339,
-    title: "Uncanny Avengers (2012) #1",
-    characters: [
-      "Captain America",
-      "Havok",
-      "Rogue",
-      "Scarlet Witch",
-      "Thor",
-      "Wolverine",
-    ],
-  },
-];
+    title: 'Uncanny Avengers (2012) #1',
+    characters: ['Captain America', 'Havok', 'Rogue', 'Scarlet Witch', 'Thor', 'Wolverine']
+  }
+]
+
+export const Home = () => {
+  const [filter, setFilter] = useState('')
+
+  const filteredComics = comics.filter(comic => comic.title.toLowerCase().includes(filter.toLowerCase()))
+
+  return (
+    <main className="container">
+      <Header />
+      <ComicList comics={filteredComics} filter={filter} onFilter={setFilter} />
+      <Footer itemsCount={filteredComics.length} />
+    </main>
+  )
+}
 
 export const Header = () => {
   return (
     <header>
-      <h1 className="title">Buscador de cómics de Marvel</h1>
+      <h1 className="title">
+        Buscador de cómics de Marvel
+      </h1>
       <h2 className="subtitle">
-        Este buscador encontrará los cómics en los que aparezcan los dos
-        personajes que selecciones en el formulario
+        Este buscador encontrará los cómics en los que aparezcan los dos personajes que selecciones en el formulario
       </h2>
     </header>
-  );
-};
+  )
+}
 
-export const Listado = ({ filtro }) => {
-
+export const ComicList = ({ filter, onFilter, comics }) => {
   return (
     <section>
-      {comics.filter(o => o.title.toLocaleLowerCase().includes(filtro.toLocaleLowerCase())).map((comic) => (
+      <p className="inputLabel">
+        Escribe un personaje en la lista
+      </p>
+      <div className="inputContainer">
+        <input className="filterInput" onInput={e => onFilter(e.target.value)} value={filter} />
+        <button className="clearButton" onClick={() => onFilter('')}>Limpiar búsqueda</button>
+      </div>
+      {comics.map(comic => (
         <div key={comic.id} className="comicCard">
-          <p className="comicTitle">{comic.title}</p>
-          <p>{comic.characters.join(", ")}</p>
+          <p className="comicTitle">
+            {comic.title}
+          </p>
+          <p>{comic.characters.join(', ')}</p>
         </div>
       ))}
     </section>
-  );
-};
+  )
+}
 
-export const Footer = () => {
+export const Footer = ({ itemsCount }) => {
   return (
     <footer>
-      <p>Footer</p>
+      <p>Elementos en la lista: {itemsCount}</p>
     </footer>
-  );
-};
+  )
+}
 
-export const Home = () => {
-  const [text, setText] = useState("");
-  return (
-    <main className="container">
-      <Header />
-      <p className="inputLabel">Escribe un personaje en la lista</p>
-      <SearchBox text={text} setText={setText} placeholder="Escribe el titulo" />
-      <Listado filtro={text} />
-      <Footer />
-    </main>
-  );
-};
+
+
+
+
+

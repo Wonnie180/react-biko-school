70790f324df1f035168e8119593c1fda1a3c9e18
Luis José Llamas Pérez <luisjllamasperez@gmail.com>
2022-09-24 12:08:32 +0200

    Solucion

--- src/ui/App.js
+++ src/ui/App.js
@@ -1,4 +1,31 @@
-import './_styles/index'
-import { Home } from './Home/Home'
+import "./_styles/index";
+import { Home } from "./Home/Home";
+import { createContext, useState } from "react";
 
-export const App = () => <Home />
+export const ThemeContext = createContext({
+  themes: [],
+  themeId: 0,
+  modifyTheme: () => {},
+});
+
+export const ThemeProvider = ({ children }) => {
+  const themesLabel = { 0: "claro", 1: "oscuro" };
+  const themes = { 0: "container", 1: "container_black" };
+  const [themeId, setThemeId] = useState(0);
+
+  // probar localstorage sincrono 
+  
+  return (
+    <ThemeContext.Provider
+      value={{ themesLabel, themes, themeId, modifyTheme: setThemeId }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const App = () => (
+  <ThemeProvider>
+    <Home />
+  </ThemeProvider>
+);
--- src/ui/Home/Home.js
+++ src/ui/Home/Home.js
@@ -1,134 +1,166 @@
-import './Home.css'
-import React, { useEffect, useState } from 'react'
-import { api } from '../../api'
+import "./Home.css";
+import React, { useEffect, useState, useContext } from "react";
+import { api } from "../../api";
+import { ThemeContext } from "../App";
 
 export const Home = () => {
-  const [firstCharacterSelect, setFirstCharacterSelect] = useState()
-  const [secondCharacterSelect, setSecondCharacterSelect] = useState()
-  const [characters, setCharacters] = useState([])
-  const [comics, setComics] = useState([])
+  const [firstCharacterSelect, setFirstCharacterSelect] = useState();
+  const [secondCharacterSelect, setSecondCharacterSelect] = useState();
+  const [characters, setCharacters] = useState([]);
+  const [comics, setComics] = useState([]);
+
+  const { themes, themeId } = useContext(ThemeContext);
+
+  console.log(themes, themeId);
+
+  useEffect(() => {
+    console.log("Cambiado el tema");
+  }, [themeId]);
 
   useEffect(() => {
     const fetchCharacters = async () => {
-      const characters = await api.characters()
-      setCharacters(characters)
-    }
+      const characters = await api.characters();
+      setCharacters(characters);
+    };
 
-    fetchCharacters()
-  }, [])
+    fetchCharacters();
+  }, []);
 
   useEffect(() => {
-    if (!firstCharacterSelect || !secondCharacterSelect) return
+    if (!firstCharacterSelect || !secondCharacterSelect) return;
 
     const getCommonComics = async () => {
-      const firstCharacterComics = await api.comics(firstCharacterSelect)
-      const secondCharacterComics = await api.comics(secondCharacterSelect)
+      const firstCharacterComics = await api.comics(firstCharacterSelect);
+      const secondCharacterComics = await api.comics(secondCharacterSelect);
 
-      const commonComics = firstCharacterComics.filter(comic1 => secondCharacterComics.some(comic2 => comic1.id === comic2.id))
+      const commonComics = firstCharacterComics.filter((comic1) =>
+        secondCharacterComics.some((comic2) => comic1.id === comic2.id)
+      );
 
-      setComics(commonComics)
-    }
+      setComics(commonComics);
+    };
 
-    getCommonComics()
-  }, [firstCharacterSelect, secondCharacterSelect])
+    getCommonComics();
+  }, [firstCharacterSelect, secondCharacterSelect]);
 
   const clearSearch = () => {
-    setFirstCharacterSelect(undefined)
-    setSecondCharacterSelect(undefined)
-    setComics([])
-  }
+    setFirstCharacterSelect(undefined);
+    setSecondCharacterSelect(undefined);
+    setComics([]);
+  };
 
   return (
-    <main className="container">
+    <main className={themes[themeId]}>
       <Header />
-      <ComicList characters={characters}
-                 firstCharacterSelect={firstCharacterSelect}
-                 secondCharacterSelect={secondCharacterSelect}
-                 onFirstCharacterSelect={setFirstCharacterSelect}
-                 onSecondCharacterSelect={setSecondCharacterSelect}
-                 comics={comics}
-                 onClear={clearSearch}
+      <ComicList
+        characters={characters}
+        firstCharacterSelect={firstCharacterSelect}
+        secondCharacterSelect={secondCharacterSelect}
+        onFirstCharacterSelect={setFirstCharacterSelect}
+        onSecondCharacterSelect={setSecondCharacterSelect}
+        comics={comics}
+        onClear={clearSearch}
       />
       <Footer itemsCount={comics.length} />
     </main>
-  )
-}
+  );
+};
 
 const Header = () => {
+  const { themesLabel, themeId, modifyTheme } = useContext(ThemeContext);
+
+  const themeLabel = themesLabel[themeId];
+
   return (
     <header>
       <div className="checkboxContainer">
-        <span className="checkboxLabel">El tema actual es: claro</span>
-        <input className="checkbox" type="checkbox" />
+        <span className="checkboxLabel">El tema actual es: {themeLabel}</span>
+        <input
+          className="checkbox"
+          type="checkbox"
+          onChange={(e) => {
+            if (e.target.checked) {
+              modifyTheme(1);
+            } else {
+              modifyTheme(0);
+            }
+          }}
+        />
       </div>
-      <h1 className="title">
-        Buscador de cómics de Marvel
-      </h1>
+      <h1 className="title">Buscador de cómics de Marvel</h1>
       <h2 className="subtitle">
-        Este buscador encontrará los cómics en los que aparezcan los dos personajes que selecciones en el formulario
+        Este buscador encontrará los cómics en los que aparezcan los dos
+        personajes que selecciones en el formulario
       </h2>
     </header>
-  )
-}
+  );
+};
 
 const ComicList = ({
-                     comics,
-                     characters,
-                     firstCharacterSelect,
-                     secondCharacterSelect,
-                     onFirstCharacterSelect,
-                     onSecondCharacterSelect,
-                     onClear
-                   }) => {
-  const selectOptions = characters.map(character => ({ value: character.id, label: character.name }))
+  comics,
+  characters,
+  firstCharacterSelect,
+  secondCharacterSelect,
+  onFirstCharacterSelect,
+  onSecondCharacterSelect,
+  onClear,
+}) => {
+  const selectOptions = characters.map((character) => ({
+    value: character.id,
+    label: character.name,
+  }));
 
   return (
     <section>
-      <p className="inputLabel">
-        Selecciona una pareja de personajes
-      </p>
+      <p className="inputLabel">Selecciona una pareja de personajes</p>
       <div className="inputContainer">
-        <Select options={selectOptions} value={firstCharacterSelect} onChange={onFirstCharacterSelect} />
-        <Select options={selectOptions} value={secondCharacterSelect} onChange={onSecondCharacterSelect} />
-        <button className="clearButton" onClick={() => onClear()}>Limpiar búsqueda</button>
+        <Select
+          options={selectOptions}
+          value={firstCharacterSelect}
+          onChange={onFirstCharacterSelect}
+        />
+        <Select
+          options={selectOptions}
+          value={secondCharacterSelect}
+          onChange={onSecondCharacterSelect}
+        />
+        <button className="clearButton" onClick={() => onClear()}>
+          Limpiar búsqueda
+        </button>
       </div>
-      {comics.map(comic => (
+      {comics.map((comic) => (
         <div key={comic.id} className="comicCard">
-          <p className="comicTitle">
-            {comic.title}
-          </p>
-          <p>{comic.characters.join(', ')}</p>
+          <p className="comicTitle">{comic.title}</p>
+          <p>{comic.characters.join(", ")}</p>
         </div>
       ))}
     </section>
-  )
-}
+  );
+};
 
 const Footer = ({ itemsCount }) => {
   return (
     <footer>
       <p>Elementos en la lista: {itemsCount}</p>
     </footer>
-  )
-}
+  );
+};
 
-const Select = ({ options, value = '', onChange }) => {
+const Select = ({ options, value = "", onChange }) => {
   return (
-    <select className="characterSelector" value={value} onChange={e => onChange(e.target.value)}>
+    <select
+      className="characterSelector"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    >
       <option value="" />
-      {
-        options.map(option => {
-          return <option key={option.value} value={option.value}>{option.label}</option>
-        })
-      }
+      {options.map((option) => {
+        return (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        );
+      })}
     </select>
-  )
-}
-
-
-
-
-
-
-
-
+  );
+};
